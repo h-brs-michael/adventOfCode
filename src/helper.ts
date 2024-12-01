5376af8df069f9fa27f221faa26df5b00108d1fb
michael <klein_michi@gmx.net>
2024-12-01 08:53:13 +0100

    Adjust helper

--- src/helper.ts
+++ src/helper.ts
@@ -1,25 +1,25 @@
 import * as fs from "fs";
 import * as readline from "readline";
 
-export async function readFile(dateiPfad: string, print = false): Promise<string[]> {
-    try {
-      const leseStream: fs.ReadStream = fs.createReadStream(dateiPfad, "utf8");
-  
-      const rl: readline.Interface = readline.createInterface({
-        input: leseStream,
-        crlfDelay: Infinity, // Unterstützt CRLF (\r\n) und LF (\n)
-      });
-  
-      const content = [];
+export async function readFile(path: string, debug = false): Promise<string[]> {
+  try {
+    const readStream: fs.ReadStream = fs.createReadStream(path, "utf8");
 
-      for await (const line of rl) {
+    const rl: readline.Interface = readline.createInterface({
+      input: readStream,
+      crlfDelay: Infinity, // Unterstützt CRLF (\r\n) und LF (\n)
+    });
+
+    const content = [];
+    for await (const line of rl) {
+      if (debug) {
         console.log(`line: ${line}`);
-        content.push(line);
       }
-      return content;
-  
-    } catch (err: unknown) {
-      console.error("error:", err);
-      return [];
+      content.push(line);
     }
+    return content;
+  } catch (err: unknown) {
+    console.error("error:", err);
+    return [];
   }
+}
